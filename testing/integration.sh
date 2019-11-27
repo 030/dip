33f2084ca8bab8a2a165674ab659250f61eb3996
030 <chocolatey030@gmail.com>
2019-11-27 14:58:12 +0100

    [GH-22] integration test

---
+++ testing/integration.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+registry(){
+    docker run -d --rm -p 5000:5000 --name registry registry:2
+}
+
+test(){
+    ./dip.exe -image sonatype/nexus3 -registry localhost:5000/ -latest "^(\d+\.){2}\d$" -preserve
+    ./dip.exe -image traefik -registry localhost:5000/ -latest "^v(\d+\.){1,2}\d+$" -preserve
+}
+
+cleanup(){
+    docker stop registry
+}
+
+main(){
+    registry
+    test
+}
+
+trap cleanup EXIT
+main
