d8d7f367eb1f549ad30ca2bc34accf63232025e8
ben <chocolatey030@gmail.com>
2020-02-13 08:27:23 +0100

    [GH-38] test command function

--- main_test.go
+++ main_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+func TestCommand(t *testing.T) {
+	// happy
+	got := command("lls")
+	if got != nil {
+		t.Errorf("Want 'no error', got %v", got)
+	}
+	// unhappy
+	got = command("hello")
+	want := "Cannot run: 'hello'. Error: 'bash: hello: command not found\n', exit status 127"
+	if want != got.Error() {
+		t.Errorf("Want %v, got %v", want, got)
+	}
+}
