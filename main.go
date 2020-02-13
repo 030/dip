475066554494c461c1f8ee68fda326f4c9775bb2
030 <chocolatey030@gmail.com>
2020-02-13 11:05:17 +0100

    [GH-38] test happy and unhappy flow of 'tags' function

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"os/exec"
 	"regexp"
 	"sort"
 	"strings"
-	"time"
 
 	"github.com/buger/jsonparser"
 	"github.com/hashicorp/go-version"
@@ -28,6 +25,62 @@ func command(s string) error {
 		return fmt.Errorf("Cannot run: '%s'. Error: '%s', %v", s, outString, err)
 	}
 	return nil
+}
+
+// tags returns tag json formatted information from dockerhub
+func tags(image string) ([]string, error) {
+	// if image does not contain a forward slash, the assumption is that it
+	// is a library
+	log.Debug("Checking whether image: '" + image + "' is a library")
+	if !strings.Contains(image, "/") {
+		log.Info("Image: '" + image + "' is a library. Concatenating 'library/'...")
+		image = "library/" + image
+	}
+
+	log.Debug("Getting raw tag information on dockerhub for image: '" + image + "'")
+
+	// get url
+	resp, err := http.Get("https://registry.hub.docker.com/v2/repositories/" + image + "/tags?page_size=1024")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// read body into bytes
+	var b []byte
+	if resp.StatusCode == http.StatusOK {
+		log.Debug("Dockerhub api call Ok. Reading body as []byte")
+		b, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// get tags and return them as a slice
+	// var c string
+	var arr []string
+	_, err = jsonparser.ArrayEach(b, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+
+		key := "name"
+
+		s, _ := jsonparser.GetString(value, key)
+		if s == "" {
+			log.Warning("No value retrieved for key: '%s'", key)
+		}
+
+		arr = append(arr, s)
+		log.Debug(arr)
+
+	}, "results")
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(arr) == 0 {
+		return nil, fmt.Errorf("No versions were found. Check whether image '" + image + "' exists in the registry")
+	}
+	return arr, nil
 }
 
 // absent checks whether a specific docker image is absent
@@ -54,21 +107,6 @@ func absent(image, registry string) bool {
 	}
 
 	return false
-}
-
-// tags returns tag json formatted information from dockerhub
-func tags(image string) *http.Response {
-	// if image does not contain a forward slash, the assumption is that it
-	// is a library
-	if !strings.Contains(image, "/") {
-		image = "library/" + image
-	}
-
-	resp, err := http.Get("https://registry.hub.docker.com/v2/repositories/" + image + "/tags?page_size=1024")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return resp
 }
 
 // readResp reads a http response and returns it as byte
@@ -134,87 +172,87 @@ func latestTag(b []byte, t string) (string, error) {
 	return sortedLatest(arr), nil
 }
 
-func main() {
-	debug := flag.Bool("debug", false, "Whether debug mode should be enabled.")
-	image := flag.String("image", "", "Find an image on dockerhub, e.g. nginx:1.17.5-alpine or nginx.")
-	latest := flag.String("latest", "", "The regex to get the latest tag, e.g. \"xenial-\\d.*\".")
-	registry := flag.String("registry", "", "To what destination the image should be transferred, e.g. quay.io/some-org/. Note: do not omit the last forward slash.")
-	preserve := flag.Bool("preserve", false, "Whether an image from dockerhub should be stored in a private registry.")
-	date := flag.Bool("date", false, "Sometimes the version of an image gets overwritten by the community due to security updates. In order to store the latest image in a private registry, one could append a date.")
+// func main() {
+// 	debug := flag.Bool("debug", false, "Whether debug mode should be enabled.")
+// 	image := flag.String("image", "", "Find an image on dockerhub, e.g. nginx:1.17.5-alpine or nginx.")
+// 	latest := flag.String("latest", "", "The regex to get the latest tag, e.g. \"xenial-\\d.*\".")
+// 	registry := flag.String("registry", "", "To what destination the image should be transferred, e.g. quay.io/some-org/. Note: do not omit the last forward slash.")
+// 	preserve := flag.Bool("preserve", false, "Whether an image from dockerhub should be stored in a private registry.")
+// 	date := flag.Bool("date", false, "Sometimes the version of an image gets overwritten by the community due to security updates. In order to store the latest image in a private registry, one could append a date.")
 
-	flag.Parse()
+// 	flag.Parse()
 
-	if *debug {
-		log.SetLevel(log.DebugLevel)
-	}
+// 	if *debug {
+// 		log.SetLevel(log.DebugLevel)
+// 	}
 
-	var latestDetectedTag string
-	if *latest != "" {
-		l, err := latestTag(readResp(tags(*image)), *latest)
-		if err != nil {
-			log.Fatal(err)
-		}
+// 	var latestDetectedTag string
+// 	if *latest != "" {
+// 		l, err := latestTag(readResp(tags(*image)), *latest)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
 
-		latestDetectedTag = ":" + l
-		fmt.Println(latestDetectedTag)
-	}
+// 		latestDetectedTag = ":" + l
+// 		fmt.Println(latestDetectedTag)
+// 	}
 
-	i := *image + latestDetectedTag
-	// forward slashes are not allowed in some registries like quay.io,
-	// e.g. sonatype/nexus3:3.19.1 will become sonatype-nexus3:3.19.1
-	i = strings.Replace(i, "/", "-", -1)
+// 	i := *image + latestDetectedTag
+// 	// forward slashes are not allowed in some registries like quay.io,
+// 	// e.g. sonatype/nexus3:3.19.1 will become sonatype-nexus3:3.19.1
+// 	i = strings.Replace(i, "/", "-", -1)
 
-	var d string
-	if *date {
-		currentTime := time.Now()
-		d = i + "-" + currentTime.Format("20060102-150405")
-	} else {
-		d = i
-	}
+// 	var d string
+// 	if *date {
+// 		currentTime := time.Now()
+// 		d = i + "-" + currentTime.Format("20060102-150405")
+// 	} else {
+// 		d = i
+// 	}
 
-	if *registry != "" {
-		dockerImageAbsent := absent(d, *registry)
-		if !dockerImageAbsent {
-			msg := "Docker image: " + d + " already exists in registry: " + *registry
-			if *preserve {
-				// Never return an exit1 if the aim is to preserve an image as
-				// the CI will become RED, while it should be green if an image
-				// is already present
-				log.Info(msg)
-				os.Exit(0)
-			} else {
-				// Return an Exit1 if an image already exists to prevent that
-				// it gets overwritten if tagImmutability is absent in a
-				// docker registry
-				log.Fatal(msg)
-			}
-		} else {
-			log.Info("Docker image: ", d, " does NOT exist in registry: ", *registry)
-		}
-	}
+// 	if *registry != "" {
+// 		dockerImageAbsent := absent(d, *registry)
+// 		if !dockerImageAbsent {
+// 			msg := "Docker image: " + d + " already exists in registry: " + *registry
+// 			if *preserve {
+// 				// Never return an exit1 if the aim is to preserve an image as
+// 				// the CI will become RED, while it should be green if an image
+// 				// is already present
+// 				log.Info(msg)
+// 				os.Exit(0)
+// 			} else {
+// 				// Return an Exit1 if an image already exists to prevent that
+// 				// it gets overwritten if tagImmutability is absent in a
+// 				// docker registry
+// 				log.Fatal(msg)
+// 			}
+// 		} else {
+// 			log.Info("Docker image: ", d, " does NOT exist in registry: ", *registry)
+// 		}
+// 	}
 
-	if *preserve {
-		var cmd string
+// 	if *preserve {
+// 		var cmd string
 
-		cmd = "docker pull " + *image + latestDetectedTag
-		log.Info(cmd)
-		err := command(cmd)
-		if err != nil {
-			log.Fatal(err)
-		}
+// 		cmd = "docker pull " + *image + latestDetectedTag
+// 		log.Info(cmd)
+// 		err := command(cmd)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
 
-		cmd = "docker tag " + *image + latestDetectedTag + " " + *registry + d
-		log.Info(cmd)
-		err = command(cmd)
-		if err != nil {
-			log.Fatal(err)
-		}
+// 		cmd = "docker tag " + *image + latestDetectedTag + " " + *registry + d
+// 		log.Info(cmd)
+// 		err = command(cmd)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
 
-		cmd = "docker push " + *registry + d
-		log.Info(cmd)
-		err = command(cmd)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
+// 		cmd = "docker push " + *registry + d
+// 		log.Info(cmd)
+// 		err = command(cmd)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// }
--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ func TestCommand(t *testing.T) {
 	if got != nil {
 		t.Errorf("Want 'no error', got %v", got)
 	}
+
 	// unhappy
 	got = command("hello")
 	want := "Cannot run: 'hello'. Error: 'bash: hello: command not found\n', exit status 127"
@@ -20,9 +21,17 @@ func TestCommand(t *testing.T) {
 
 func TestTags(t *testing.T) {
 	// happy
-	got := tags("mongo").StatusCode
-	want := 200
-	if got != want {
-		t.Errorf("Want '%v', got %v", want, got)
+	got, _ := tags("mongo")
+	gotInt := len(got)
+	wantGreaterThan := 0
+	if !(gotInt > wantGreaterThan) {
+		t.Errorf("Mongo exists in dockerhub and thus the number of tags should be greater than 0. Want greater than '%d', got '%d'.", wantGreaterThan, gotInt)
+	}
+
+	// unhappy
+	_, err := tags("image-does-not-exist")
+	want := "No versions were found. Check whether image 'library/image-does-not-exist' exists in the registry"
+	if want != err.Error() {
+		t.Errorf("Want '%v', got '%v'", want, err)
 	}
 }
