84ac4baa6f4b1fb5ab6398ec127c882edcd32a65
ben <chocolatey030@gmail.com>
2020-02-13 09:26:38 +0100

    [GH-38] test happy flow tags

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCommand(t *testing.T) {
 	// happy
-	got := command("lls")
+	got := command("ls")
 	if got != nil {
 		t.Errorf("Want 'no error', got %v", got)
 	}
@@ -13,5 +15,14 @@ func TestCommand(t *testing.T) {
 	want := "Cannot run: 'hello'. Error: 'bash: hello: command not found\n', exit status 127"
 	if want != got.Error() {
 		t.Errorf("Want %v, got %v", want, got)
+	}
+}
+
+func TestTags(t *testing.T) {
+	// happy
+	got := tags("mongo").StatusCode
+	want := 200
+	if got != want {
+		t.Errorf("Want '%v', got %v", want, got)
 	}
 }
