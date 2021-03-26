a46f0bd9406f1d5e8c1bb56c5ee08e6d984baaf5
ben <chocolatey030@gmail.com>
2021-03-26 21:00:54 +0100

    [GH-52] Ensure that latest update version will be returned if applicable.

--- internal/sort/tags.go
+++ internal/sort/tags.go
@@ -30,13 +30,13 @@ func Tags(tags []string) (string, error) {
 			continue
 		}
 
-		reg, err := regexp.Compile("[_a-zA-Z-]+")
-		if err != nil {
-			return "", err
-		}
 		log.Debugf("tag: '%v'", tag)
-		tagWithoutChars := reg.ReplaceAllString(tag, "")
+
+		re := regexp.MustCompile(`((\d+\.){2}\d+)`)
+		match := re.FindStringSubmatch(tag)
+		tagWithoutChars := match[1]
 		log.Debugf("tagWithoutChars: '%v'", tagWithoutChars)
+
 		semver := regexp.MustCompile("\\.").Split(tagWithoutChars, 3)
 		major, err := strconv.Atoi(semver[0])
 		if err != nil {
@@ -51,6 +51,30 @@ func Tags(tags []string) (string, error) {
 			return "", err
 		}
 		version := major*1e6 + minor*1e3 + patch
+
+		if val, ok := m[version]; ok {
+			log.Debugf("Version: '%d' with Tag: '%s' resides already in map!", version, val)
+
+			u1, err := update(val)
+			if err != nil {
+				return "", err
+			}
+			log.Debug(u1)
+
+			u2, err := update(tag)
+			if err != nil {
+				return "", err
+			}
+			log.Debug(u1)
+			if u1 == 0 || u2 == 0 {
+				continue
+			}
+			if u1 > u2 {
+				tag = val
+			}
+			log.Debug(tag)
+		}
+
 		m[version] = tag
 	}
 	log.Debugf("Map: '%v'", m)
@@ -71,4 +95,20 @@ func Tags(tags []string) (string, error) {
 	}
 
 	return latestTag, nil
+}
+
+func update(tag string) (int, error) {
+	re := regexp.MustCompile(`(_|-\w+\d\.)(\d+)`)
+	match := re.FindStringSubmatch(tag)
+	elements := len(match)
+	if elements != 2 {
+		log.Debugf("Cannot determine update number. Required elements is two, but was: '%d'", elements)
+		return 0, nil
+	}
+	updateString := match[2]
+	u, err := strconv.Atoi(updateString)
+	if err != nil {
+		return 0, err
+	}
+	return u, nil
 }
--- test/integration-tests.sh
+++ test/integration-tests.sh
@@ -3,14 +3,19 @@
 source ./scripts/build.sh
 cd cmd/dip
 
+readonly DIP_ERROR="Cannot find the latest tag. Check whether the tags are semantic"
+
 echo -e "\nAdoptopenjdk"
 ./"${DIP_DELIVERABLE}" -image=adoptopenjdk -latest="14.*-jre-hotspot-bionic" -official
 
 echo -e "\nAlpine"
 ./"${DIP_DELIVERABLE}" -image alpine -latest "(\d+\.){2}\d" --official | grep "[0-9]"
 
+echo -e "\nGolang"
+./"${DIP_DELIVERABLE}" -image=golang -latest "^1\..*-alpine[0-9]+\.[0-9]+$" -official | grep "[0-9]"
+
 echo -e "\nMinio"
-./"${DIP_DELIVERABLE}" -image minio/minio -latest "RELEASE\.2019.*" | grep "Cannot find the latest tag. Check whether the tags are semantic" | grep "[0-9]" || true
+./"${DIP_DELIVERABLE}" -image minio/minio -latest "RELEASE\.2019.*" | grep "${DIP_ERROR}" | grep "[0-9]" || true
 
 echo -e "\nNginx"
 ./"${DIP_DELIVERABLE}" -image=nginx -latest=1\..* -official | grep "[0-9]"
@@ -25,4 +30,4 @@ echo -e "\nTraefik"
 ./"${DIP_DELIVERABLE}" --image=traefik --latest="^v(\d+\.){1,2}\d+$" -official | grep "[0-9]"
 
 echo -e "\nUbuntu"
-./"${DIP_DELIVERABLE}" -image ubuntu -latest "^xenial.*" -official | grep "[0-9]" || true
+./"${DIP_DELIVERABLE}" -image ubuntu -latest "^xenial.*" -official | grep "${DIP_ERROR}" || true
