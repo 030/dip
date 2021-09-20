f0de58ccbe47cd9f17e091279e42d557cd60263b
ben <chocolatey030@gmail.com>
2021-09-20 23:18:30 +0200

    [GH-96] Send message to slack if Dockerfile image is outdated.

--- test/integration-tests.sh
+++ test/integration-tests.sh
@@ -6,39 +6,39 @@ cd cmd/dip
 readonly DIP_ERROR="Cannot find the latest tag. Check whether the tags are semantic"
 
 echo -e "\nAdoptopenjdk"
-./"${DIP_DELIVERABLE}" -image=adoptopenjdk -latest="14.*-jre-hotspot-bionic" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=adoptopenjdk --regex="14.*-jre-hotspot-bionic" | grep "[0-9]"
 
 echo -e "\nAdoptopenjdk16"
-./"${DIP_DELIVERABLE}" -image=adoptopenjdk -latest="16.*" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=adoptopenjdk --regex="16.*" | grep "[0-9]"
 
 echo -e "\nAlpine"
-./"${DIP_DELIVERABLE}" -image alpine -latest "(\d+\.){2}\d" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=alpine --regex "(\d+\.){2}\d" | grep "[0-9]"
 
 echo -e "\nGolang"
-./"${DIP_DELIVERABLE}" -image=golang -latest "^1\..*-alpine[0-9]+\.[0-9]+$" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=golang --regex "^1\..*-alpine[0-9]+\.[0-9]+$" | grep "[0-9]"
 
 echo -e "\nMinio"
-./"${DIP_DELIVERABLE}" -image minio/minio -latest "RELEASE\.2021.*" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=minio/minio --regex "RELEASE\.2021.*" | grep "[0-9]"
 
 echo -e "\nN3DR"
 n3drTag="^6.0.10$"
-./"${DIP_DELIVERABLE}" -image=utrecht/n3dr -latest=$n3drTag | grep $n3drTag
+./"${DIP_DELIVERABLE}" image --name=utrecht/n3dr --regex=$n3drTag | grep $n3drTag
 
 echo -e "\nNginx"
-./"${DIP_DELIVERABLE}" -image=nginx -latest=1\..* | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=nginx --regex=1\..* | grep "[0-9]"
 
 echo -e "\nSonatype Nexus3"
-./"${DIP_DELIVERABLE}" -image=sonatype/nexus3 -latest=3\..* | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=sonatype/nexus3 --regex=3\..* | grep "[0-9]"
 
 echo -e "\nSonarqube"
-./"${DIP_DELIVERABLE}" -image sonarqube -latest ".*-community$" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=sonarqube --regex ".*-community$" | grep "[0-9]"
 
 echo -e "\nTraefik"
-./"${DIP_DELIVERABLE}" --image=traefik --latest="^v(\d+\.){1,2}\d+$" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=traefik --regex="^v(\d+\.){1,2}\d+$" | grep "[0-9]"
 
 echo -e "\nUbuntu"
-./"${DIP_DELIVERABLE}" -image ubuntu -latest "^impish.*" | grep "[0-9]"
+./"${DIP_DELIVERABLE}" image --name=ubuntu --regex "^impish.*" | grep "[0-9]"
 
 echo -e "\nUbuntu Hirsute"
 ubuntuHirsuteTag="^hirsute-20210522$"
-./"${DIP_DELIVERABLE}" -image ubuntu -latest $ubuntuHirsuteTag | grep $ubuntuHirsuteTag
+./"${DIP_DELIVERABLE}" image --name=ubuntu --regex $ubuntuHirsuteTag | grep $ubuntuHirsuteTag
